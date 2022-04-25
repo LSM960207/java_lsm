2cbbbe1c81b97a4deeb390b4badfe9dc2322d699
LSM960207 <104331382+LSM960207@users.noreply.github.com>
2022-04-25 16:51:19 +0900

    자바 1일차 : 연습 예제

---
+++ java/src/day1/Ex5_Test.java
@@ -0,0 +1,27 @@
+package day1;
+
+public class Ex5_Test {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/* 1. 과일가게에서 과일 판매량을 기록하려 한다.
+		 * 사과, 오렌지의 판매량을 기록하기 위한 변수를 선언하세요.
+		 */
+		int apple = 0;
+		int orange = 0;
+		
+		/* 2-1. 학생 한 명의 국어, 영어, 수학 성적을 저장하려 한다.
+		 * 이때 필요한 변수를 선언하세요. (조건 : 성적은 정수로 관리된다.)
+		 */
+		int koScore = 0;
+		int engScore = 0;
+		int mathScore = 0;
+		
+		
+		/* 2-2. 대학생의 학점을 출력하려고 한다.
+		 * 	   이 때 학점을 저장하는 변수를 선언하세요. (조건 : 학점은 F~A로 or 0~4.5로)
+		 */
+		float score1 = 0.0f;
+		char score2 = 'F'; 
+	}
+
+}
