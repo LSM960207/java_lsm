d3a4303ea7299e08292d92144daaed6b9ddd802d
LSM960207 <104331382+LSM960207@users.noreply.github.com>
2022-04-26 12:32:20 +0900

    자바 2일차 : 비교연산자

---
+++ java/src/day2/Ex5_Comparison.java
@@ -0,0 +1,21 @@
+package day2;
+
+public class Ex5_Comparison {
+
+	public static void main(String[] args) {
+		/* >= 반대로 쓰면 안됨
+		 * <= 반대로 쓰면 안됨
+		 * ==를 =과 혼동하면 안됨
+		 * 결과 값이 참 또는 거짓
+		 * 비교 연산자는 연산자를 기준으로 양쪽에 어떤 수가 나옴
+		 */
+		int score = 95;
+		System.out.println(score > 95);
+		System.out.println(score >= 95);
+		System.out.println(score < 95);
+		System.out.println(score <= 95);
+		System.out.println(score != 95);
+
+	}
+
+}
