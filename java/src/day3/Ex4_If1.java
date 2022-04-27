6fa4e44ececc4ee2e3bd9a9380b08eaa775e2a5d
LSM960207 <104331382+LSM960207@users.noreply.github.com>
2022-04-27 13:58:50 +0900

    자바 3일차 : 산술 연산자 입력 예제(Scanner)

---
+++ java/src/day3/Ex4_If1.java
@@ -0,0 +1,42 @@
+package day3;
+
+import java.util.Scanner;
+
+public class Ex4_If1 {
+
+	public static void main(String[] args) {
+		/* 두 정수와 산술 연산자를 입력받아 산술 연산 결과를 출력하는 코드를 작성하세요.
+		 * Scanner3의 방법2 예제 코드를 가져옴
+		 * 
+		 * op1이 +이면 num3 + num4를 출력하고,
+		 * op1이 -이면 num3 - num4를 출력하고,
+		 * op1이 *이면 num3 * num4를 출력하고,
+		 * op1이 /이면 num3 / num4를 출력하고,
+		 * op1이 %이면 num3 % num4를 출력하고,
+		 * op1이 산술 연산자가 아니면 op1은 산술 연산자가 아니라고 출력한다.
+		 * */
+
+		Scanner scan = new Scanner(System.in);
+		System.out.print("두 정수와 산술 연산자를 입력하세요 (예 : 1 + 2) : ");
+		int num3 = scan.nextInt();
+		char op1 = scan.next().charAt(0);
+		int num4 = scan.nextInt();
+		
+		if(op1 == '+') {
+			System.out.println(""+num3 + op1 + num4+"="+(num3 + num4));
+		}else if(op1 == '-') {
+			System.out.println(""+num3 + op1 + num4+"="+(num3 - num4));
+		}else if(op1 == '*') {
+			System.out.println(""+num3 + op1 + num4+"="+(num3 * num4));
+		}else if(op1 == '/') {
+			System.out.println(""+num3 + op1 + num4+"="+((double)num3 / num4));
+		}else if(op1 == '%') {
+			System.out.println(""+num3 + op1 + num4+"="+(num3 % num4));
+		}else {
+			System.out.println(op1+"는 산술 연산자가 아닙니다.");
+		}
+		scan.close();
+		
+	}
+
+}
