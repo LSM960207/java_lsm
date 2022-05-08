9a62c8538d73656734ed212d40c672fd7144f895
LSM960207 <104331382+LSM960207@users.noreply.github.com>
2022-05-08 23:33:09 +0900

    자바 9일차 : 클래스 예제 - 학생 예제

--- java/src/day9/Ex4_Class1_Student.java
+++ java/src/day9/Ex4_Class1_Student.java
@@ -3,14 +3,15 @@ package day9;
 public class Ex4_Class1_Student {
 
 	public static void main(String[] args) {
-		HighSchoolStudent std = new HighSchoolStudent("청석", "임석민", 1, 9, 27);
+		HighSchoolStudent std = new HighSchoolStudent("운호", "전일규", 1, 10, 23);
+		std.print();
 
 	}
 
 }
-/* 고등학생 클래스에 필요한 필드를 주석으로 써보고 선언해보세요.*/
-class HighSchoolStudent {
-	// 학교명, 성명, 학년, 반, 번호, 국어, 영어, 수학
+/* 고등학생 클래스에 필요한 필드를 주석으로 써 보고 선언해보세요. */
+class HighSchoolStudent{
+	//학교이름, 이름, 학년, 반, 번호, 국어, 영어, 수학
 	String schoolName;
 	String name;
 	int grade;
@@ -21,11 +22,11 @@ class HighSchoolStudent {
 	int math;
 	
 	void print() {
-		System.out.println("고등학교 : " + schoolName);
-		System.out.println("이름 	  : " + name);
-		System.out.println("학년 	  : " + grade);
-		System.out.println("학반   : " + classNumber);
-		System.out.println("번호   : " + number);
+		System.out.println("고등학교 : " + schoolName );
+		System.out.println("이름    : " + name);
+		System.out.println("학년    : " + grade);
+		System.out.println("학반    : " + classNumber);
+		System.out.println("번호    : " + number);
 	}
 	
 	public HighSchoolStudent(String schoolName, String name, int grade, int classNumber, int number) {
@@ -35,5 +36,5 @@ class HighSchoolStudent {
 		this.classNumber = classNumber;
 		this.number = number;
 	}
-		
+	
 }
