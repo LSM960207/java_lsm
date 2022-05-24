00c27440dcb0c79841c09c36a548ae0cda969c3b
LSM960207 <104331382+LSM960207@users.noreply.github.com>
2022-05-24 17:17:41 +0900

    자바 19일차 : 게시글 관리 프로그램 예제

---
+++ java/src/day19/Ex02_BoardMain.java
@@ -0,0 +1,35 @@
+package day19;
+
+import java.util.Scanner;
+
+public class Ex02_BoardMain {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		BoardManager bm = new BoardManager(scan);
+		bm.run();
+		/* 1번 메뉴를 선택 : 게시글 등록 기능을 실행
+		 * 타입 입력 
+		 * 제목 입력
+		 * 내용 입력
+		 * 작성자 입력
+		 * 게시글 생성 후 저장
+		 * 
+		 * 2번 메뉴를 선택 : 게시글 확인 기능을 실행
+		 * 전체 게시글 확인
+		 * 게시글 선택
+		 * 게시글 상세 확인
+		 * 
+		 * 3번 메뉴를 선택 : 게시글 수정 기능을 실행
+		 * 전체 게시글 확인
+		 * 게시글 선택
+		 * 게시글 제목 입력
+		 * 게시글 내용 입력
+		 * 게시글 수정
+		 * 
+		 * 4번 메뉴를 선택 : 프로그램을 종료 */
+	
+		
+	}
+
+}
