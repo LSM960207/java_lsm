d5636056fc06204dd82df669989219f326d5f5da
LSM960207 <104331382+LSM960207@users.noreply.github.com>
2022-05-30 11:23:38 +0900

    자바 24일차 : 전화번호 관리 프로그램 예제

---
+++ java/src/day24/Ex01_PhoneMain.java
@@ -0,0 +1,13 @@
+package day24;
+
+import java.util.*;
+
+public class Ex01_PhoneMain {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		PhoneManager pm = new PhoneManager(scan);
+		pm.run();
+		scan.close();
+	}
+}
