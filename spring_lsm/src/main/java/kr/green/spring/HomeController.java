4084752f84a0188aeb5c1c2740b1b518a2ddaae9
LSM960207 <104331382+LSM960207@users.noreply.github.com>
2022-08-02 16:02:11 +0900

    spring : 스프링 데이터 전송 연습

--- spring_lsm/src/main/java/kr/green/spring/HomeController.java
+++ spring_lsm/src/main/java/kr/green/spring/HomeController.java
@@ -1,6 +1,7 @@
 package kr.green.spring;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -28,10 +29,30 @@ public class HomeController {
 	 * method : url 요청 방식, RequestMethod.GET/RequestMethod.POST
 	 * 					생략하면 둘 다 처리
 	 * */
-	@RequestMapping(value="/")
-	public ModelAndView home(ModelAndView mv) {
+	/* 화면에서 hobby와 time을 안보내면, null이 자동으로 들어감
+	 * 이 때, hobby와 time의 타입이 null을 처리할 수 없는 타입이면 에러가 발생
+	 * */
+	@RequestMapping(value="/", method=RequestMethod.GET)
+	public ModelAndView home(ModelAndView mv, String hobby, Integer time) {
 		mv.setViewName("/main/home");
-		mv.addObject("setHeader", "타일즈");
+		mv.addObject("name", "홍길동");
+		mv.addObject("age", 20);
+		System.out.println("취미는 " + hobby + "이고, " + time + "시간씩 합니다.");
+		return mv;
+	}
+	@RequestMapping(value="/", method=RequestMethod.POST)
+	public ModelAndView homePost(ModelAndView mv, String hobby, Integer time) {
+		mv.setViewName("redirect:/");
+		System.out.println("취미는 " + hobby + "이고, " + time + "시간씩 합니다.");
+		return mv;
+	}
+	
+	@RequestMapping(value="/hobby/{hobby1}/{time1}")
+	public ModelAndView hobby(ModelAndView mv,
+		@PathVariable("hobby1") String hobby,
+		@PathVariable("time1") Integer time) {
+		mv.setViewName("redirect:/");
+		System.out.println("취미는 " + hobby + "이고, " + time + "시간씩 합니다.");
 		return mv;
 	}
 	
