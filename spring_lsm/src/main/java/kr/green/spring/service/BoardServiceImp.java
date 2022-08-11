b705a5952c8585f82ffba2f99a0c20aea5d3e964
LSM960207 <104331382+LSM960207@users.noreply.github.com>
2022-08-11 17:10:45 +0900

    spring : 게시글 추천/비추천 기능 구현 완료(선택된 추천/비추천 색상 미적용)

--- spring_lsm/src/main/java/kr/green/spring/service/BoardServiceImp.java
+++ spring_lsm/src/main/java/kr/green/spring/service/BoardServiceImp.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import kr.green.spring.dao.BoardDAO;
 import kr.green.spring.pagination.Criteria;
 import kr.green.spring.vo.BoardVO;
+import kr.green.spring.vo.LikesVO;
 import kr.green.spring.vo.MemberVO;
 
 @Service
@@ -78,6 +79,7 @@ public class BoardServiceImp implements BoardService{
 			return;
 		
 		boardDao.updateBoard(board);
+		
 	}
 
 	@Override
@@ -108,4 +110,36 @@ public class BoardServiceImp implements BoardService{
 		return boardDao.selectTotalCount(cri);
 	}
 
+	@Override
+	public String updateLikes(LikesVO likes) {
+		if(likes == null)
+			return "";
+		//조회 => 로그인한 사용자가 해당 게시물에 한 추천/비추천 정보를 가져옴
+		LikesVO dbLikes = boardDao.selectLikes(likes);
+		try {
+			//해당 게시물에 추천/비추천이 처음이면 insert
+			if(dbLikes == null) {
+				boardDao.insertLikes(likes);
+				return "" + likes.getLi_state();
+				//처음이 아니고 한 적이 있으면 update를 해야함
+			}else {
+				//이전 추천/비추천 상태와 현재 추천/비추천 상태가 다른 경우
+				//이전 추천 => 현재 비추천, 이전 비추천 => 현재 추천
+				if(dbLikes.getLi_state() != likes.getLi_state()) {
+					dbLikes.setLi_state(likes.getLi_state());
+				}
+				//이전 추천/비추천 상태가 현재와 같은 경우 => 상태가 0
+				//이전 추천 => 현재 추천, 이전 비추천 => 현재 비추천
+				else {
+					dbLikes.setLi_state(0);
+				}
+				boardDao.updateLikes(dbLikes);
+				return likes.getLi_state()+(dbLikes.getLi_state() == 0 ? "0" : "");//-10, 10
+			}		
+		}catch(Exception e) {}
+		finally {
+			boardDao.updateBoardLikes(likes.getLi_bd_num());
+		}
+		return "";
+	}
 }
