35a1a7e336c3a67e89297aa610c93c05af41b0e1
LSM960207 <104331382+LSM960207@users.noreply.github.com>
2022-08-12 11:34:32 +0900

    spring 복습 : 비밀번호 암호화

--- spring_lsm_test/src/main/java/kr/green/springtest/controller/HomeController.java
+++ spring_lsm_test/src/main/java/kr/green/springtest/controller/HomeController.java
@@ -3,6 +3,7 @@ package kr.green.springtest.controller;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
